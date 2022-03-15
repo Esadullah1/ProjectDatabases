ed845196be3c182e193a495140a8b19af7ce1992
Esadullah1 <45165761+Esadullah1@users.noreply.github.com>
2022-03-15 20:21:48 +0100

    sommeren

---
+++ Someren-master/SomerenDAL/StudentDao.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using SomerenModel;
+
+namespace SomerenDAL
+{
+    public class StudentDao : BaseDao
+    {      
+        public List<Student> GetAllStudents()
+        {
+ 
+            string query = "SELECT StudentID, FirstName, DateOfBirth FROM [Students]";
+
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Student> ReadTables(DataTable dataTable)
+        {
+            List<Student> students = new List<Student>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Student student = new Student()
+                {
+                    studentID = (int)dr["StudentID"],
+                    studentName = (string)(dr["FirstName"].ToString()),
+                    DateOfBirth = (DateTime)dr["DateOfBirth"]
+                };
+                students.Add(student);
+            }
+            return students;
+        }
+    }
+}
